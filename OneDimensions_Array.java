2595aa0575ed8f4d7164a43c82deec91b0d98fe2
Ankit Kumar <65466117+Maa7700@users.noreply.github.com>
2021-08-31 08:43:47 +0530

    Add files via upload

---
+++ OneDimensions_Array.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class OneDimensions_Array {
+
+	public static void main(String[] args) {
+		
+		int[]num= {1,2,3};
+		
+		for(int i=0;i<3;i++) 
+		{
+			
+			System.out.println(num[i]);
+	
+	}
+
+}
+}
+
+/*
+Array- Its a data structure which holds sequential element of same data type.
+It represent multiple data items as a list.
+Its of 2 types - One dimensions and Multi dimension array.
+If more than one value is there we need to use array.
+
+*/
